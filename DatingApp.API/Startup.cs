f85b9e6ef29dff74afd7bbc6a2c7ab21295b0f5d
Shailesh Bistul Mhatre <mhatre.shailesh@hotmail.com>
2019-01-08 08:03:42 +0100

    End Of Section 13

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -57,7 +57,7 @@ namespace DatingApp.API
                             ValidateAudience = false
                         };
                     });
-            
+             services.AddScoped<LogUserActivity>();
 
         }
 
